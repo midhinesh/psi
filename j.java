f291defa22a49895875cdd6747ce2b17d8f4c48e
midhinesh <79622607+midhinesh@users.noreply.github.com>
2021-02-25 11:16:46 +0530

    s

---
+++ j.java
@@ -0,0 +1,9 @@
+public class j{
+	public static void main(String[] args)
+	{
+		//using the max() method of Math class
+		System.out.print("The maximum number is: " + Math.max(9,4564));
+	}
+}
+
+		
